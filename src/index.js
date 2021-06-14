d8827e92a558fb2feaa48886880f08b852533b32
volodymyr-oliinyk8 <oliynyk.vo@gmail.com>
2021-06-15 00:07:36 +0300

    start base elements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './sass/main.scss';
 
 import './scripts/first-visit-loading';
+import './scripts/logic-row-search-for-name';
---
+++ src/scripts/logic-row-search-for-name.js
@@ -0,0 +1,12 @@
+import NewApiService from './apiService';
+import getRefs from './get-refs';
+
+const refs = getRefs();
+
+const apiService = new NewApiService();
+
+function feachEventSearchForName() {
+    
+}
+
+    
