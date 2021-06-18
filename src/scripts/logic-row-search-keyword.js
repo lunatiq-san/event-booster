41410af8ae67e1b7f02b2049bb5b585a1f142751
volodymyr-oliinyk8 <oliynyk.vo@gmail.com>
2021-06-19 00:04:02 +0300

    bugfix render on bad search

--- src/scripts/logic-row-search-keyword.js
+++ src/scripts/logic-row-search-keyword.js
@@ -1,7 +1,8 @@
 import NewApiService from './apiService';
 import getRefs from './get-refs';
 import debounce from 'lodash.debounce';
-import eventsTpl from '../templates/events.hbs'
+import eventsTpl from '../templates/events.hbs';
+import Swal from 'sweetalert2';
 
 const refs = getRefs();
 
@@ -11,15 +12,29 @@ refs.searchQuery.addEventListener('input', debounce(onSearch, 500));
 
 function onSearch(e) {
     apiService.query = e.target.value;
-    feachEventSearchKeyWord();
+    apiService.fetchEventsDefault().then(renderEventsCard).catch(onError);
 }
 
-function feachEventSearchKeyWord() {
-    apiService.fetchEventsDefault().then(renderEventsCard);
+function onError() {
+    Swal.fire('Oops...', 'Nothing was found for your query! Try again...', 'error');
+      resetInput();
+      resetKeyword();
+      resetPage();
 }
+// function feachEventSearchKeyWord() {
+//     apiService.fetchEventsDefault().then(renderEventsCard).catch(console.log('qwert'));
+// }
 
 function renderEventsCard(events) {
     const markup = eventsTpl(events);
     refs.eventsList.innerHTML = markup;
 }
-    
+function resetInput() {
+    refs.searchQuery.value = '';
+}
+function resetKeyword() {
+    apiService.query = "";
+}
+function resetPage() {
+    apiService.fetchEventsDefault().then(renderEventsCard);
+}
