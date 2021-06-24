45a59595959421e202bfd21889751082db2e8161
Rozumiak Vladyslav <lunatiq86@gmail.com>
2021-06-24 15:05:52 +0300

    Fixed all styles

--- src/scripts/event-details.js
+++ src/scripts/event-details.js
@@ -10,68 +10,54 @@ refs.eventsList.addEventListener('click', openLightboxOnClick);
 function openLightboxOnClick(e) {
   let card = e.target.closest('li');
   if (!card) return;
+
   renderEventCard(card);
-  refs.lightbox.classList.add("is-open");
-  document.body.classList.add("is-hidden");
+  refs.lightbox.classList.add('is-open');
+  document.body.classList.add('is-hidden');
   addEventListeners();
-  
-};
+}
 
 function renderEventCard(card) {
   apiService.id = card.dataset.id;
-  apiService.searchEventById()
+  apiService
+    .searchEventById()
     .then(currentEvent => {
       if (currentEvent.dates.start.localTime) {
         currentEvent.dates.start.localTime = currentEvent.dates.start.localTime.slice(0, -3);
-      };
+      }
       refs.lightboxContent.insertAdjacentHTML('afterbegin', eventTpl(currentEvent));
     })
     .catch(error => console.log(error));
-};
+}
 
 function closeLightboxOnClick() {
-  refs.lightbox.classList.remove("is-open");
-  document.body.classList.remove("is-hidden")
+  refs.lightbox.classList.remove('is-open');
+  document.body.classList.remove('is-hidden');
   removeEventListeners();
   refs.lightboxContent.innerHTML = ' ';
-    
-};
+}
 
 function closeLightboxOnEscKeyPress(e) {
   const isEscKey = e.code === ESC_KEY_CODE;
   if (isEscKey) {
     closeLightboxOnClick();
   }
-};
+}
 
 function onLightboxBackdropClick(e) {
   if (e.target === e.currentTarget) {
     closeLightboxOnClick();
-    };
-
-};
+  }
+}
 
 function addEventListeners() {
-   window.addEventListener('keydown', closeLightboxOnEscKeyPress);
+  window.addEventListener('keydown', closeLightboxOnEscKeyPress);
   refs.closeLightboxBtn.addEventListener('click', closeLightboxOnClick);
   refs.lightboxBackdrop.addEventListener('click', onLightboxBackdropClick);
-};
+}
 
 function removeEventListeners() {
   window.removeEventListener('keydown', closeLightboxOnEscKeyPress);
   refs.closeLightboxBtn.removeEventListener('click', closeLightboxOnClick);
   refs.lightboxBackdrop.removeEventListener('click', onLightboxBackdropClick);
-};
-
-
-
-
-// refs.eventsList.addEventListener('keydown', openLightboxOnEnterKeydown);
-
-// function openLightboxOnEnterKeydown(e) {
-//   console.log(e.code);
-//   console.log(e.target.classList.contains('event-card'));
-//   if (e.code === 'Enter' && e.target.classList.contains('event-card')) {
-//     openLightboxOnClick(e);
-//   }
-// }
+}
